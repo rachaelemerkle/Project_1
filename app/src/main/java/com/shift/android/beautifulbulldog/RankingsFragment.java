16dc0bf24506643f26a49c4271c7717d553531f8
Rachael Merkle <rachaelemerkle@gmail.com>
2017-09-27 22:03:24 -0500

    'final_project_1'

--- app/src/main/java/com/shift/android/beautifulbulldog/RankingsFragment.java
+++ app/src/main/java/com/shift/android/beautifulbulldog/RankingsFragment.java
@@ -1,18 +1,22 @@
 package com.shift.android.beautifulbulldog;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ListView;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class RankingsFragment extends Fragment {
-
+    private ListView ratingList;
 
     public RankingsFragment() {
         // Required empty public constructor
@@ -23,7 +27,33 @@ public class RankingsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_rankings, container, false);
+        View view=  inflater.inflate(R.layout.fragment_rankings, container, false);
+
+        ratingList= (ListView) view.findViewById(R.id.rating_list);
+        MainActivity mainActivity = (MainActivity) this.getActivity();
+
+        BulldogArrayAdapter adapter = new BulldogArrayAdapter(this.getActivity(), mainActivity.realm.where(Bulldog.class).findAll());
+        ratingList.setAdapter(adapter);
+
+        ratingList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {final Bulldog bulldog = (Bulldog) adapterView.getItemAtPosition(i);
+                Intent intent = new Intent(view.getContext(),BulldogActivity.class);
+                intent.putExtra("bulldog",bulldog.getId());
+                startActivity(intent);
+            }
+        });
+        return view;
+
     }
 
 }
+
+
+
+
+
+
+
+
+
